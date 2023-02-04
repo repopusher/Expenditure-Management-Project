304488098e73bd82bf1876b1df441bbb04453056
repopusher <tobiaseastwoodlennon@gmail.com>
2023-02-04 19:25:42 +0000

    Camera API
    Camera is previewed after user logs in

--- FYP_Mobile/app/src/main/java/com/example/fyp_mobile/MainActivity.kt
+++ FYP_Mobile/app/src/main/java/com/example/fyp_mobile/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.example.fyp_mobile
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Patterns
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import java.util.regex.Pattern
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val loginButton = findViewById<Button>(R.id.loginButton)
+        val emailEditText = findViewById<EditText>(R.id.emailEditText)
+        val passwordEditText = findViewById<EditText>(R.id.passwordEditText)
+
+        loginButton.setOnClickListener {
+            if(emailEditText.text.toString().isBlank()){
+                emailEditText.requestFocus()
+                emailEditText.error = "Field cannot be left empty"
+            }
+
+            else if(passwordEditText.text.isBlank()){
+                passwordEditText.requestFocus()
+                passwordEditText.error = "Field cannot be left empty"
+            }
+
+            else {
+                if (!emailEditText.text.toString().isValidEmail()) {
+                    emailEditText.requestFocus()
+                    emailEditText.error = "Enter a valid email"
+                }
+                else {
+//                    TODO AUTHENTICATE LOGIN WHEN DB CONNECTION IS IMPLEMENTED
+                    val intent = Intent(this, Camera::class.java)
+//                    intent.putExtra("key", value)
+                    startActivity(intent)
+                }
+            }
+        }
     }
+    private fun String.isValidEmail() =
+        !TextUtils.isEmpty(this) && Patterns.EMAIL_ADDRESS.matcher(this).matches()
 }
