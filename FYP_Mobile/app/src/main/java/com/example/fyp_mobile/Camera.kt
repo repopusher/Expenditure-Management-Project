5b301ea6d674db5a627432b4859be3709467adb0
repopusher <tobiaseastwoodlennon@gmail.com>
2023-02-05 21:05:27 +0000

    Camera functionality added

--- FYP_Mobile/app/src/main/java/com/example/fyp_mobile/Camera.kt
+++ FYP_Mobile/app/src/main/java/com/example/fyp_mobile/Camera.kt
@@ -2,102 +2,69 @@ package com.example.fyp_mobile
 
 import android.Manifest
 import android.annotation.SuppressLint
-import android.content.Context
+import android.content.ActivityNotFoundException
+import android.content.Intent
 import android.content.pm.PackageManager
-import android.graphics.SurfaceTexture
-import android.hardware.camera2.CameraCaptureSession
-import android.hardware.camera2.CameraDevice
-import android.hardware.camera2.CameraManager
-import android.hardware.camera2.CaptureRequest
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.HandlerThread
-import android.view.Surface
-import android.view.TextureView
-import androidx.core.app.ActivityCompat
-import org.w3c.dom.Text
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 
+@Suppress("DEPRECATION")
 class Camera : AppCompatActivity() {
 
-    lateinit var handler: Handler
-    lateinit var handlerThread: HandlerThread
-    lateinit var cameraManager: CameraManager
-    lateinit var cameraCaptureSession: CameraCaptureSession
-    lateinit var cameraDevice: CameraDevice
-    lateinit var captureRequestBuild: CaptureRequest.Builder
-    lateinit var captureRequest: CaptureRequest
+    private lateinit var captureButton: Button
+    private lateinit var confirmButton: Button
+    private lateinit var resultTextView: TextView
+    private val REQUEST_IMAGE_CAPTURE = 101
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
         // Requesting camera permissions for camera API
+        captureButton = findViewById(R.id.captureButton)
+        confirmButton = findViewById(R.id.confirmButton)
+        resultTextView = findViewById(R.id.textView)
+
+
         getPermissions()
 
-        val textureView = findViewById<TextureView>(R.id.textureView)
+        //Capture button listener dispatches camera intent
+        captureButton.setOnClickListener {
+            dispatchTakePictureIntent()
+        }
 
-        cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
-        handlerThread = HandlerThread("videoThread")
-        handlerThread.start()
-        handler = Handler((handlerThread).looper)
+    }
 
-        textureView.surfaceTextureListener = object: TextureView.SurfaceTextureListener {
-            override fun onSurfaceTextureAvailable(p0: SurfaceTexture, p1: Int, p2: Int) {
-                openCamera(textureView)
-            }
-
-            override fun onSurfaceTextureSizeChanged(p0: SurfaceTexture, p1: Int, p2: Int) {
-            }
-
-            override fun onSurfaceTextureDestroyed(p0: SurfaceTexture): Boolean {
-                return false
-            }
-
-            override fun onSurfaceTextureUpdated(p0: SurfaceTexture) {
-            }
-
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 101){
+            //Image captured in BitMap format
+            var pic = data?.getParcelableExtra<Bitmap>("data")
         }
     }
 
-    //Test
-    @SuppressLint("MissingPermission")
-    fun openCamera(textureView: TextureView){
-        cameraManager.openCamera(cameraManager.cameraIdList[0], @SuppressLint("MissingPermission")
-        object: CameraDevice.StateCallback(){
-            override fun onOpened(p0: CameraDevice) {
-                cameraDevice = p0
-
-                captureRequestBuild = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
-                val surface = Surface(textureView.surfaceTexture)
-                captureRequestBuild.addTarget(surface)
-
-                cameraDevice.createCaptureSession(listOf(surface), object: CameraCaptureSession.StateCallback(){
-                    override fun onConfigured(p0: CameraCaptureSession) {
-                        cameraCaptureSession = p0
-
-                        cameraCaptureSession.setRepeatingRequest(captureRequestBuild.build(), null, null)
-                    }
-
-                    override fun onConfigureFailed(p0: CameraCaptureSession) {
-                    }
-
-                }, handler)
-             }
-
-            override fun onDisconnected(p0: CameraDevice) {
-                TODO("Not yet implemented")
-            }
-
-            override fun onError(p0: CameraDevice, p1: Int) {
-                TODO("Not yet implemented")
-            }
-
-        }, handler)
-    }
 
     private fun getPermissions(){
-        val permission = arrayOf(android.Manifest.permission.CAMERA)
-        requestPermissions(permission, 1122)
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            val permission = arrayOf(Manifest.permission.CAMERA)
+            requestPermissions(permission, 1122)
+        }
+    }
+
+    private fun dispatchTakePictureIntent() {
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        try {
+            startActivityForResult(cameraIntent, REQUEST_IMAGE_CAPTURE)
+        } catch (e: ActivityNotFoundException) {
+            Toast.makeText(this, "An error occurred, please try again", Toast.LENGTH_SHORT).show()
+        }
     }
 
 }
