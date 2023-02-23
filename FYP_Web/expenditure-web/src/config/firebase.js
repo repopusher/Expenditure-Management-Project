9b1383164f4427fdceafc6de70eeac8b0aab2309
repopusher <tobiaseastwoodlennon@gmail.com>
2023-02-23 18:17:13 +0000

    Register, signup, reset password implemented

--- FYP_Web/expenditure-web/src/config/firebase.js
+++ FYP_Web/expenditure-web/src/config/firebase.js
@@ -27,12 +27,13 @@ const logInWithEmailAndPassword = async (email, password) => {
   }
 };
 
-const registerWithEmailAndPassword = async (email, password) => {
+const registerWithEmailAndPassword = async (name, email, password) => {
   try {
     const res = await createUserWithEmailAndPassword(auth, email, password);
     const user = res.user;
     await addDoc(collection(db, "users"), {
       uid: user.uid,
+      name,
       authProvider: "local",
       email,
     });
