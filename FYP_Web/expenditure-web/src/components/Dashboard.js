6dcd6f406ac7a385a5d8a555f712af41a67e6e65
repopusher <tobiaseastwoodlennon@gmail.com>
2023-02-25 15:54:13 +0000

    update

--- FYP_Web/expenditure-web/src/components/Dashboard.js
+++ FYP_Web/expenditure-web/src/components/Dashboard.js
@@ -8,18 +8,33 @@ function Dashboard() {
   //Error variable if you need it in [user, loading, error]
   const [user, loading] = useAuthState(auth);
   const [name, setName] = useState("");
+  const collectionReceiptReference = collection(db, "receipts")
+  const collectionUserReference = collection(db, "users")
 
   const navigate = useNavigate();
 
   const fetchUserName = async () => {
     try {
-      const q = query(collection(db, "users"), where("uid", "==", user?.uid));
+      const q = query(collectionUserReference, where("uid", "==", user?.uid));
       const doc = await getDocs(q);
       const data = doc.docs[0].data();
       setName(data.name);
     } catch (err) {
       console.error(err);
       alert("An error occured while fetching user data");
+    }
+  };
+
+  const fetchUserReceipts = async () => {
+    try {
+      const q = query(collectionReceiptReference, where("uid", "==", user?.uid));
+      const doc = await getDocs(q);
+      const data = doc.docs[0].data();
+
+      console.log(data.item, data.date, data.total);
+    } catch (err) {
+      console.log(err);
+      alert("An error occured while fetching receipt data");
     }
   };
 
@@ -31,6 +46,7 @@ function Dashboard() {
         if (!user) return navigate('/', {replace: true});
 
         fetchUserName();
+        fetchUserReceipts();
     }
 
     fetchBusinesses()
