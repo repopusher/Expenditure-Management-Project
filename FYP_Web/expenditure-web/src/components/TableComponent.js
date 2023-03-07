a7d925742a3d0d31d42df2a0982a0609687684f9
repopusher <tobiaseastwoodlennon@gmail.com>
2023-03-07 23:04:23 +0000

    Dynamically rendered table implemented

--- FYP_Web/expenditure-web/src/components/TableComponent.js
+++ FYP_Web/expenditure-web/src/components/TableComponent.js
@@ -21,58 +21,85 @@ const useStyles = makeStyles({
   },
 });
 
-function createData(item, quantity, price, date) {
-  return { item, quantity, price, date };
-}
 
-const rows = [
-  createData('Item 1', 1, 10.99, '2022-01-01'),
-  createData('Item 2', 2, 20.99, '2022-01-02'),
-  createData('Item 3', 3, 30.99, '2022-01-03'),
-];
-
-function TableComponent() {
+function TableComponent(props) {
   const classes = useStyles();
   const [value, setValue] = useState(0);
+  const { receipts } = props;
 
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
+  const getCategoryTabIndex = (category) => {
+    switch (category) {
+      case "groceries":
+        return 0;
+      case "utilities":
+        return 1;
+      case "transport":
+        return 2;
+      default:
+        return 3;
+    }
   };
 
   return (
     <div className={classes.root}>
-      <Tabs value={value} onChange={handleChange} centered className={classes.tabs}>
+      <Tabs value={value} onChange={(event, newValue) => setValue(newValue)} centered className={classes.tabs}>
         <Tab label="Groceries" />
         <Tab label="Utilities" />
         <Tab label="Transport" />
         <Tab label="Other" />
       </Tabs>
-      <TableContainer component={Paper} className={classes.container}>
-        <Table stickyHeader aria-label="sticky table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Item</TableCell>
-              <TableCell align="right">Quantity</TableCell>
-              <TableCell align="right">Price</TableCell>
-              <TableCell align="right">Date</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.map((row) => (
-              <TableRow key={row.item}>
-                <TableCell component="th" scope="row">
-                  {row.item}
-                </TableCell>
-                <TableCell align="right">{row.quantity}</TableCell>
-                <TableCell align="right">{row.price}</TableCell>
-                <TableCell align="right">{row.date}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+      
+      {[...Array(4).keys()].map((index) => (
+        <TabPanel value={value} index={index} key={index}>
+          <TableContainer component={Paper} className={classes.container}>
+            <Table stickyHeader aria-label="sticky table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Item</TableCell>
+                  <TableCell align="right">Quantity</TableCell>
+                  <TableCell align="right">Price</TableCell>
+                  <TableCell align="right">Date</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {receipts.map((receipt) => {
+                  if (getCategoryTabIndex(receipt.category) !== index) return null;
+                  return receipt.items.map((item, index) => (
+                    <TableRow key={index}>
+                      <TableCell component="th" scope="row">
+                        {item}
+                      </TableCell>
+                      <TableCell align="right">{receipt.quantities[index]}</TableCell>
+                      <TableCell align="right">{receipt.totals[index]}</TableCell>
+                      <TableCell align="right">{receipt.date}</TableCell>
+                    </TableRow>
+                  ));
+                })}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </TabPanel>
+      ))}
     </div>
   );
 }
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div role="tabpanel" hidden={value !== index} id={`scrollable-auto-tabpanel-${index}`} aria-labelledby={`scrollable-auto-tab-${index}`} {...other}>
+      {value === index && (
+        <div>
+          {children}
+        </div>
+      )}
+    </div>
+  );
+}
+
+TableComponent.propTypes = {
+  receipts: PropTypes.array.isRequired,
+};
 
 export default TableComponent;
