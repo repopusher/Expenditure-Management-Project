ad028d7ba4e9d8c63362b831ce1b53169d10e670
repopusher <tobiaseastwoodlennon@gmail.com>
2023-03-12 21:39:19 +0000

    update/delete-component-impl

--- FYP_Web/expenditure-web/src/components/Dashboard.js
+++ FYP_Web/expenditure-web/src/components/Dashboard.js
@@ -1,73 +1,70 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useMemo, useCallback } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useNavigate } from "react-router-dom";
 import { auth, db, logout } from "../config/firebase";
 import { query, collection, getDocs, where } from "firebase/firestore";
 import TableComponent from "./TableComponent";
+import ReceiptEntryForm from "./ReceiptEntryForm";
 
 function Dashboard() {
-  //Error variable if you need it in [user, loading, error]
   const [user, loading] = useAuthState(auth);
   const [name, setName] = useState("");
-  const [receipts, setReceipts] = useState([]); // State variable for receipts
-  const collectionReceiptReference = collection(db, "receipts")
-  const collectionUserReference = collection(db, "users")
+  const [receipts, setReceipts] = useState([]);
+  const [rowData , setRowData] = useState({});
 
   const navigate = useNavigate();
 
-  const fetchUserName = async () => {
-    try {
-      const q = query(collectionUserReference, where("uid", "==", user?.uid));
-      const doc = await getDocs(q);
-      const data = doc.docs[0].data();
-      setName(data.name);
-    } catch (err) {
-      console.error(err);
-      alert("An error occured while fetching user data");
-    }
-  };
-
-  const mapReceiptData = (data) => {
-
-    const receipt = {
+  const mapReceiptData = useMemo(() => (data) => {
+    return {
+      id: data.id,
       uid: data.uid,
       category: data.category,
       date: data.date,
       items: data.items,
       quantities: data.quantities,
       totals: data.totals
-    }
-    return receipt;
-  };
+    };
+  }, []);
 
-  const fetchUserReceipts = async () => {
-    try {
-      const q = query(collectionReceiptReference, where("uid", "==", user?.uid));
-      const doc = await getDocs(q);
-      const data = doc.docs.map((doc) => mapReceiptData(doc.data()));
-      return data;
-    } catch (err) {
-      console.error(err);
-      alert("An error occured while fetching user data");
-    }
-  };
+  const handleRowClick = useCallback((event, rowData) => {
+    setRowData(rowData);
+  }, []);
 
   useEffect(() => {
-    async function fetchBusinesses() {
-      if (loading) return;
-      if (!user) return navigate('/', {replace: true});
-      fetchUserName();
-      const data = await fetchUserReceipts(); // Call fetchUserReceipts and wait for the returned data
-      setReceipts(data); // Set the receipts state variable to the returned data
+    async function fetchUserData() {
+      try {
+        if (!user) return navigate('/', {replace: true});
+
+        const qName = query(collection(db, "users"), where("uid", "==", user?.uid));
+        const qReceipts = query(collection(db, "receipts"), where("uid", "==", user?.uid));
+
+        const [nameDoc, receiptsDoc] = await Promise.all([getDocs(qName), getDocs(qReceipts)]);
+
+        const nameData = nameDoc.docs[0].data();
+        setName(nameData.name);
+
+        const receiptData = receiptsDoc.docs.map((doc) => {
+        const receipt = mapReceiptData(doc.data());
+        receipt.id = doc.id;
+        return receipt;
+        });
+        setReceipts(receiptData);
+      } catch (err) {
+        console.error(err);
+        alert("An error occured while fetching user data");
+      }
     }
-    fetchBusinesses()
-  }, [user, loading, navigate]);
+
+    fetchUserData();
+  }, [user, navigate, mapReceiptData]);
 
   return (
     <div className="dashboard">
       <div className="dashboard__container">
         <p>Logged in as </p>
-        <TableComponent receipts={receipts} /> {/* Pass the receipts state variable as a prop */}
+        <TableComponent receipts={receipts} onRowClick={handleRowClick} />
+        {Object.keys(rowData).length !== 0 && <ReceiptEntryForm rowData={rowData} />}
+
         <div>{name}</div>
         <div>{user?.email}</div>
         <button className="dashboard__btn" onClick={logout}>
